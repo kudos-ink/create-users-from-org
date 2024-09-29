14d51d2c7843b2fdf3dc873a69847a710fc56db1
Leandro Palazzolo <leapalazzolo@gmail.com>
2024-09-29 19:59:32 -0300

    feat: add avatar

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod config;
 mod model;
 
 use config::Configuration;
+use model::CreatedUser;
 use octocrab::Octocrab;
 use reqwest::Client;
 
@@ -76,13 +77,15 @@ async fn main() {
     let endpoint = config.issues_api + "/users";
     for member in all_members {
         println!("Creating member {}", member.login);
-
+        let user = &model::User {
+            username: member.login.clone(),
+            avatar: member.avatar_url.to_string(),
+        };
+        println!("user {:?}", user);
         let response = client
             .post(&endpoint)
             .header("Authorization", &config.issues_api_token)
-            .json(&model::User {
-                username: member.login.clone(),
-            })
+            .json(user)
             .send()
             .await;
 
@@ -93,7 +96,50 @@ async fn main() {
             Ok(resp) => match resp.text().await {
                 Ok(msg) => {
                     if msg.contains("already exists") {
-                        eprintln!("User already exists {}", member.login);
+                        eprintln!("User already exists {}. Updating it", member.login);
+                        match client
+                            .get(endpoint.clone() + "/username/" + &member.login)
+                            .header("Authorization", &config.issues_api_token)
+                            .json(user)
+                            .send()
+                            .await
+                        {
+                            Ok(res) => {
+                                let created_user: CreatedUser = res.json().await.expect("error");
+                                let response = client
+                                    .put(endpoint.clone() + "/" + &created_user.id.to_string())
+                                    .header("Authorization", &config.issues_api_token)
+                                    .json(user)
+                                    .send()
+                                    .await;
+                                match response {
+                                    Ok(resp) => {
+                                        if resp.status().is_success() {
+                                            println!(
+                                                "Successfully updated data for user: {}",
+                                                member.login
+                                            );
+                                        } else {
+                                            println!("Error: {:?}", resp);
+                                        }
+                                    }
+                                    Err(error) => {
+                                        eprintln!(
+                                            "Error updating data for member: {:?}. Error: {:#?}",
+                                            member, error
+                                        );
+                                        process::exit(8);
+                                    }
+                                }
+                            }
+                            Err(error) => {
+                                eprintln!(
+                                    "Error updating data for member: {:?}. Error: {:#?}",
+                                    member, error
+                                );
+                                process::exit(8);
+                            }
+                        }
                     } else {
                         eprintln!("Failed to post data for member {}: {}", member.login, msg);
                         process::exit(6);
--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,13 @@
+use serde::Deserialize;
 use serde_derive::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct User {
     pub username: String,
+    pub avatar: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CreatedUser {
+    pub id: i32,
 }
